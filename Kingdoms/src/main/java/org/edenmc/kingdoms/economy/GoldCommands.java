8e1fd29fcfd3682f21ad850405bcb671acd4e2f4
JackFlukinger <jackflukinger@gmail.com>
2018-03-06 23:44:02 -0600

    Added /gold give

--- Kingdoms/src/main/java/org/edenmc/kingdoms/economy/GoldCommands.java
+++ Kingdoms/src/main/java/org/edenmc/kingdoms/economy/GoldCommands.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.edenmc.kingdoms.Kingdoms;
 import org.edenmc.kingdoms.citizen.Citizen;
@@ -15,8 +16,10 @@ public class GoldCommands implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (!(sender instanceof Player)) return false;
-        Citizen p = Kingdoms.getCitizen(sender.getName());
+        Citizen p = null;
+        if (sender instanceof Player) {
+            p = Kingdoms.getCitizen(sender.getName());
+        }
         if (cmd.getName().equalsIgnoreCase("gold")) {
             if (args.length == 3) {
                 if (args[0].equals("set") && p.getPlayer().hasPermission("gold.set")) {
@@ -28,6 +31,20 @@ public class GoldCommands implements CommandExecutor {
                             recip.setBalance(newBal);
                         } catch (NumberFormatException e) {
                             p.getPlayer().sendMessage("§bNot a valid integer");
+                            e.printStackTrace();
+                        }
+                    } else {
+                        p.getPlayer().sendMessage("§b" + args[1] + " is not a valid player");
+                    }
+                } else if (args[0].equals("give") && sender instanceof ConsoleCommandSender) {
+                    if (Bukkit.getServer().getPlayerExact(args[1]) != null) {
+                        Player recipient = Bukkit.getServer().getPlayerExact(args[1]);
+                        Citizen recip = Kingdoms.getCitizen(recipient.getName());
+                        try {
+                            Integer newBal = recip.getBalance() + Integer.parseInt(args[2]);
+                            recip.setBalance(newBal);
+                        } catch (NumberFormatException e) {
+                            sender.sendMessage("§bNot a valid integer");
                             e.printStackTrace();
                         }
                     } else {
